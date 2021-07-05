cfe7fa6c880321536a514d7ee70e08b3465fe441
PatriciaYumi <pati.yumi@yahoo.com.br>
2021-07-05 16:26:31 -0300

    DevBook att 05/06/2021

--- api/src/config/config.go
+++ api/src/config/config.go
@@ -1,5 +1,14 @@
 package config
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
 var (
 	// string conexao bd mysql
 	StringConexaoBanco = ""
@@ -9,5 +18,20 @@ var (
 
 // Inicializar variáveis de ambiente
 func Carregar() {
+	var erro error
 
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"),
+	)
 }
--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,10 +1,40 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 // Insere usuario
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando Usuário!"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarioId, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioId)))
 }
 
 // Busca todos usuarios
